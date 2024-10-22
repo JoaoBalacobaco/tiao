c246fe8bbb368156e8ce0045a07aec28827abc45
João Paulo Thomaz Belphman <149329719+JoaoBalacobaco@users.noreply.github.com>
2024-10-22 09:51:25 -0300

    Add files via upload

---
+++ tiao Carreiro/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import { Fundo } from './assets/Fundo'
+
+
+function App() {
+
+  return (
+    <div>
+      <Fundo></Fundo>
+    </div>
+  )
+}
+
+export default App
---
+++ tiao Carreiro/src/assets/Fundo.jsx
@@ -0,0 +1,9 @@
+import styles from './Fundo.moduole.css'
+
+export function Fundo() {
+  return (
+   <div >
+        <h1 className={styles.a}>aasd</h1>
+   </div>
+  );
+}
